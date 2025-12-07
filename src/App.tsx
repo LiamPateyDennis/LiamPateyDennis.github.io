a3dcb2cbed5e347cd55f62048a8777592f20157b
LiamPateyDennis <liampateydennis@gmail.com>
2025-12-08 10:12:56 +1100

    Fixed missing files, included dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,23 @@
-import Message from "./Message";
-import Alert from "./components/Alert";
-import ListGroup from "./components/ListGroup";
-import Button from "./components/Button";
-import { useState } from "react";
-
-// let items = ["New York", "San Fransico", "Tokyo", "London", "Paris"];
-
-// function App() {
-//   const handleSelectItem = (item: string) => {
-//     console.log(item);
-//   };
-
-//   return (
-//     <div>
-//       <ListGroup
-//         items={items}
-//         heading="Cities"
-//         onSelectItem={handleSelectItem}
-//       />
-//     </div>
-//   );
-// }
+import "./css/App.css";
+// import MovieCard from "./components/MovieCard";
+import Favorites from "./pages/Favorites";
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
 
 function App() {
-  const [alertVisible, setAlertVisibility] = useState(false);
   return (
-    <div>
-      {alertVisible && (
-        <Alert onClose={() => setAlertVisibility(false)}>Hello World</Alert>
-      )}
-      <Button onClick={() => setAlertVisibility(true)}>My Button</Button>
-    </div>
+    <>
+      <div>
+        <NavBar />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/favorites" element={<Favorites />} />
+          </Routes>
+        </main>
+      </div>
+    </>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import 'bootstrap/dist/css/bootstrap.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./css/index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import App from "./App.tsx";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
