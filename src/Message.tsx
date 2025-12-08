f072da4960d645b3b7b66a987c1add7abc10ecba
LiamPateyDennis <liampateydennis@gmail.com>
2025-12-08 12:53:21 +1100

    run build

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,10 +1,9 @@
-// 
+//
 
 function Message() {
-    const name = "";
-    if (name)
-        return <h1>Hello {name}</h1>
-    return <h1>Hello World</h1>
+  const name = "";
+  if (name) return <h1>Hello {name}</h1>;
+  return <h1>Hello World</h1>;
 }
 
 export default Message;
